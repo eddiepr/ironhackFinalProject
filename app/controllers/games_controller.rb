c99ed8bbc39b8272eb2c8717621d3bff09224393
Eddie Rendleman <skooltransformer2@gmail.com>
2015-07-27 18:34:12 -0400

    I have a home page with a join game link and added leave game link taking back to homepage in the join game page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,10 @@ class GamesController < ApplicationController
 	def index
 		render "index"
 	end
+	def join_game
+		render "join_game"
+	end
+	def create_game
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "/" => "games#index"
+  get "/game" => "games#join_game"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
