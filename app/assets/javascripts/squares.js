529a323c3b43d76f6a3d738aa9a7115e29615e55
Eddie Rendleman <skooltransformer2@gmail.com>
2015-07-28 11:22:10 -0400

    pushing to test ajax

--- app/assets/javascripts/squares.js
+++ app/assets/javascripts/squares.js
@@ -1,2 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('ready', function () {
+	$('.row-odd-column-odd').on('click', function () {
+		console.log("hi");
+	})
+
+
+	$.ajax({
+	    type: "GET",
+	    url: "https://multiplayer-chess.herokuapp.com/",
+	    data: '',
+	    success: onSaveSuccess,
+	    error: onSaveFailure,
+	    dataType: "json"
+	  });
+
+	function onSaveFailure () {
+		console.log("error getting chess pieces layout");
+	}
+	function onSaveSuccess (response) {
+		console.log("successs")
+	}
+
+
+})
