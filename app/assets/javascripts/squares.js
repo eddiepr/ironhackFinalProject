76d5e2abd376c8ab0bacb64518684935f0f4b2d4
Eddie Rendleman <skooltransformer2@gmail.com>
2015-07-28 17:21:40 -0400

    have rooks displaying, other glyphicons not working.  It's displaying 2 rooks in each square that it should have 1

--- app/assets/javascripts/squares.js
+++ app/assets/javascripts/squares.js
@@ -2,6 +2,8 @@
 // All this logic will automatically be available in application.js.
 
 $(document).on('ready', function () {
+
+	var piece
 	$('.row-odd-column-odd').on('click', function () {
 		console.log("hi");
 	})  
@@ -26,8 +28,36 @@ $(document).on('ready', function () {
 		response.forEach(function (item) {
 			console.log(item.x);
 			console.log(item.y);
+			// if (item.contents == "rook") {
+			// 	piece == "glyphicon glyphicon-tower"
+			// // }
+			// if (item.x == 1 && item.y == 1 && item.contents == "rook") {
+			// 	$('.row1').append('<div class=' + piece + '></div>')
+			// }
 
-
+			for (var i=1; i<9; i++) {
+				for (var j=1; j<9; j++) {
+					if (item.x == i && item.y == j && item.contents == "rook") {
+						$('.row' + i.toString() + '.column' + j.toString()).append("<div class='glyphicon glyphicon-tower'></div>")
+						//use images maybe instead of glyphicons if I can't get glyphicons to work
+					}
+					if (item.x == i && item.y == j && item.contents == "knight") {
+						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-knight"></div>')
+					}
+					if (item.x == i && item.y == j && item.contents == "bishop") {
+						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-bishop"></div>')
+					}
+					if (item.x == i && item.y == j && item.contents == "queen") {
+						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-queen"></div>')
+					}
+					if (item.x == i && item.y == j && item.contents == "king") {
+						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-king"></div>')
+					}
+					if (item.x == i && item.y == j && item.contents == "pawn") {
+						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-pawn"></div>')
+					}
+				}
+			}
 		})
 
 		//if x ==1 && y==1 { append to .div1}
