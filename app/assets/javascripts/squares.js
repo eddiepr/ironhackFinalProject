3d2ba4d177d5ec0b6dd3d24c744cb4a79e639f2d
Eddie Rendleman <skooltransformer2@gmail.com>
2015-07-28 17:47:02 -0400

    using the wrong glyhpicons because only rook glyphicon is working, the json is giving me double.  The pieces are displayed on the left and right instead of top and bottom

--- app/assets/javascripts/squares.js
+++ app/assets/javascripts/squares.js
@@ -23,38 +23,42 @@ $(document).on('ready', function () {
 		console.log("error getting chess pieces layout");
 	}
 	function onSaveSuccess (response) {
-		console.log("success");
-
+		console.log(response);
+		var number = 0;
 		response.forEach(function (item) {
-			console.log(item.x);
-			console.log(item.y);
+
+			number = number + 1;  //the json object is sending me twice as many items as are in the database for some reason
+			// console.log(number);
+			// console.log(item.x);
+			// console.log(item.y);
 			// if (item.contents == "rook") {
 			// 	piece == "glyphicon glyphicon-tower"
 			// // }
 			// if (item.x == 1 && item.y == 1 && item.contents == "rook") {
 			// 	$('.row1').append('<div class=' + piece + '></div>')
 			// }
-
-			for (var i=1; i<9; i++) {
-				for (var j=1; j<9; j++) {
-					if (item.x == i && item.y == j && item.contents == "rook") {
-						$('.row' + i.toString() + '.column' + j.toString()).append("<div class='glyphicon glyphicon-tower'></div>")
-						//use images maybe instead of glyphicons if I can't get glyphicons to work
-					}
-					if (item.x == i && item.y == j && item.contents == "knight") {
-						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-knight"></div>')
-					}
-					if (item.x == i && item.y == j && item.contents == "bishop") {
-						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-bishop"></div>')
-					}
-					if (item.x == i && item.y == j && item.contents == "queen") {
-						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-queen"></div>')
-					}
-					if (item.x == i && item.y == j && item.contents == "king") {
-						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-king"></div>')
-					}
-					if (item.x == i && item.y == j && item.contents == "pawn") {
-						$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-pawn"></div>')
+			if (number <= 64) { //the json object is sending me twice as many items as are in the database for some reason
+				for (var i=1; i<9; i++) {
+					for (var j=1; j<9; j++) {
+						if (item.y == i && item.x == j && item.contents == "rook") {
+							$('.row' + i.toString() + '.column' + j.toString()).append("<div class='glyphicon glyphicon-tower'></div>")
+							//use images maybe instead of glyphicons if I can't get glyphicons to work
+						}
+						if (item.x == i && item.y == j && item.contents == "knight") {
+							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-plane"></div>')
+						}
+						if (item.x == i && item.y == j && item.contents == "bishop") {
+							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-fullscreen"></div>')
+						}
+						if (item.x == i && item.y == j && item.contents == "queen") {
+							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-magnet"></div>')
+						}
+						if (item.x == i && item.y == j && item.contents == "king") {
+							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-globe"></div>')
+						}
+						if (item.x == i && item.y == j && item.contents == "pawn") {
+							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-leaf"></div>')
+						}
 					}
 				}
 			}
