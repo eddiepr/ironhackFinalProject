83b7abec3ef30a2eb72189dabe71614f1f6c062b
Eddie Rendleman <skooltransformer2@gmail.com>
2015-07-31 22:54:55 -0400

    join game js is only running for my join game view now

--- app/assets/javascripts/squares.js
+++ app/assets/javascripts/squares.js
@@ -1,196 +1,196 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).on('ready', function () {
+// $(document).on('ready', function () {
 
-	var piece;
-	var postingFlag = true;
-	var movingFlag = true;
-	var gameId;
-	var clickedPieceSquare;
-	var clickedPieceSquareContents;
-	var moveToSquareAndContents;
-	var contents;
-	var data;
+// 	var piece;
+// 	var postingFlag = true;
+// 	var movingFlag = true;
+// 	var gameId;
+// 	var clickedPieceSquare;
+// 	var clickedPieceSquareContents;
+// 	var moveToSquareAndContents;
+// 	var contents;
+// 	var data;
 
-	function nextLayout () {
-		piece = "reset";
-		postingFlag = true;
-		movingFlag = true;
-		gameId = "reset";
-		clickedPieceSquare = "reset";
-		clickedPieceSquareContents = "reset";
-		moveToSquareAndContents = "reset";
-		contents = "reset";
-		data = "reset";
-		console.log("did this thing");
+// 	function nextLayout () {
+// 		piece = "reset";
+// 		postingFlag = true;
+// 		movingFlag = true;
+// 		gameId = "reset";
+// 		clickedPieceSquare = "reset";
+// 		clickedPieceSquareContents = "reset";
+// 		moveToSquareAndContents = "reset";
+// 		contents = "reset";
+// 		data = "reset";
+// 		console.log("did this thing");
 
+// 		$.ajax({
+// 		    type: "GET",
+// 		    url: "/squares",
+// 		    data: '',
+// 		    success: onSaveSuccess,
+// 		    error: onSaveFailure,
+// 		    dataType: "json"
+// 		});
 
-		$.ajax({
-		    type: "GET",
-		    url: "/squares",
-		    data: '',
-		    success: onSaveSuccess,
-		    error: onSaveFailure,
-		    dataType: "json"
-		});
-	}
-	nextLayout ();
+// 	}
+// 	nextLayout ();
 
-	function appendPieces (response) {
-		var number = 0;
+// 	function appendPieces (response) {
+// 		var number = 0;
 
-		for (var i=1; i<9; i++) {
-			for (var j=1; j<9; j++) {
-				$('.row' + i.toString() + '.column' + j.toString()).empty();
-			}
-		}
-		console.log(response);
-		response.spaces.forEach(function (item) {
+// 		for (var i=1; i<9; i++) {
+// 			for (var j=1; j<9; j++) {
+// 				$('.row' + i.toString() + '.column' + j.toString()).empty();
+// 			}
+// 		}
+// 		console.log(response);
+// 		response.spaces.forEach(function (item) {
 
-			number = number + 1;  //the json object is sending me twice as many items as are in the database for some reason
+// 			number = number + 1;  //the json object is sending me twice as many items as are in the database for some reason
 			
-			if (number <= 64) { //the json object is sending me twice as many items as are in the database for some reason
-				for (var i=1; i<9; i++) {
-					for (var j=1; j<9; j++) {				
+// 			if (number <= 64) { //the json object is sending me twice as many items as are in the database for some reason
+// 				for (var i=1; i<9; i++) {
+// 					for (var j=1; j<9; j++) {				
 
-						if (item.y == i && item.x == j && item.contents == "br") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-tower" data-title="br"></div>')
-							//use images maybe instead of glyphicons if I can't get glyphicons to work
-						}
-						if (item.y == i && item.x == j && item.contents == "bn") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-plane" data-title="bn"></div>')
-						}
-						if (item.y == i && item.x == j && item.contents == "bb") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-fullscreen" data-title="bb"></div>')
-						}
-						if (item.y == i && item.x == j && item.contents == "bq") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-magnet" data-title="bq"></div>')
-						}
-						if (item.y == i && item.x == j && item.contents == "bk") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-globe king" data-title="bk"></div>')
-						}
-						if (item.y == i && item.x == j && item.contents == "bp") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-leaf pawn" data-title="bp"></div>')
-						}
+// 						if (item.y == i && item.x == j && item.contents == "br") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-tower" data-title="br"></div>')
+// 							//use images maybe instead of glyphicons if I can't get glyphicons to work
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "bn") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-plane" data-title="bn"></div>')
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "bb") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-fullscreen" data-title="bb"></div>')
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "bq") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-magnet" data-title="bq"></div>')
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "bk") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-globe king" data-title="bk"></div>')
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "bp") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-leaf pawn" data-title="bp"></div>')
+// 						}
 
 
 
-						if (item.y == i && item.x == j && item.contents == "wr") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-tower" data-title="wr"></div>')
-							//use images maybe instead of glyphicons if I can't get glyphicons to work
-						}
-						if (item.y == i && item.x == j && item.contents == "wn") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-plane" data-title="wn"></div>')
-						}
-						if (item.y == i && item.x == j && item.contents == "wb") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-fullscreen" data-title="wb"></div>')
-						}
-						if (item.y == i && item.x == j && item.contents == "wq") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-magnet" data-title="wq"></div>')
-						}
-						if (item.y == i && item.x == j && item.contents == "wk") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-globe king" data-title="wk"></div>')
-						}
-						if (item.y == i && item.x == j && item.contents == "wp") {
-							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-leaf pawn" data-title="wp"></div>')
-						}
-						$('.row' + i.toString() + '.column' + j.toString()).on('click', function () {
-							if (postingFlag == true) {
-								console.log(postingFlag);
-								postingFlag = false;
-								console.log($(this));
-								if ( $(this).children().length > 0 ) {
-									console.log(i);
-									console.log($(this).attr('data-column'));
-									console.log($(this).attr('data-row'));
-									clickedPieceSquare = {x: $(this).attr('data-column'), 
-										y: $(this).attr('data-row')
-									}
-									contents = $(this).find('[data-title]').data('title');
-									console.log(contents);
-									movingFlag = true;
-									moveToSquare (response); 	
-								}
-							}
-						})
-					}
-				}
-			}
-		})
-	}
+// 						if (item.y == i && item.x == j && item.contents == "wr") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-tower" data-title="wr"></div>')
+// 							//use images maybe instead of glyphicons if I can't get glyphicons to work
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "wn") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-plane" data-title="wn"></div>')
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "wb") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-fullscreen" data-title="wb"></div>')
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "wq") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-magnet" data-title="wq"></div>')
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "wk") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-globe king" data-title="wk"></div>')
+// 						}
+// 						if (item.y == i && item.x == j && item.contents == "wp") {
+// 							$('.row' + i.toString() + '.column' + j.toString()).append('<div class="glyphicon glyphicon-leaf pawn" data-title="wp"></div>')
+// 						}
+// 						$('.row' + i.toString() + '.column' + j.toString()).on('click', function () {
+// 							if (postingFlag == true) {
+// 								console.log(postingFlag);
+// 								postingFlag = false;
+// 								console.log($(this));
+// 								if ( $(this).children().length > 0 ) {
+// 									console.log(i);
+// 									console.log($(this).attr('data-column'));
+// 									console.log($(this).attr('data-row'));
+// 									clickedPieceSquare = {x: $(this).attr('data-column'), 
+// 										y: $(this).attr('data-row')
+// 									}
+// 									contents = $(this).find('[data-title]').data('title');
+// 									console.log(contents);
+// 									movingFlag = true;
+// 									moveToSquare (response); 	
+// 								}
+// 							}
+// 						})
+// 					}
+// 				}
+// 			}
+// 		})
+// 	}
 
-	function moveToSquarePartTwo (response) {
+// 	function moveToSquarePartTwo (response) {
 	
-		if (movingFlag == true) {
-			gameId = response.gmId;
-			var updateGamesData = {
-				id: gameId,
-				finishedStatus: false
-			}
-			var updateTurnsData = {
+// 		if (movingFlag == true) {
+// 			gameId = response.gmId;
+// 			var updateGamesData = {
+// 				id: gameId,
+// 				finishedStatus: false
+// 			}
+// 			var updateTurnsData = {
 
-			}
-			data = {
-				clickedPieceSquare: clickedPieceSquare,
-		    	updateGamesData: updateGamesData,
-		    	moveToSquareAndContents: moveToSquareAndContents
-		    }
-		    console.log(moveToSquareAndContents)
-		    console.log(clickedPieceSquare);
-			$.ajax({
-			    type: "PATCH",
-			    url: "/move",
-			    data: data,  //this is where I need to post the updated contents in games.id#.squares table.
-		    					// Also need to have a users table and update how many moves they made for that side in a 
-		    					// 	particular game.  Maybe with: Users has_many games through: newTable, Games has_many users through: newTable
-			    success: onSaveSuccessPatch,
-			    error: onSaveFailurePatch,
-			});
-		}
-	}
+// 			}
+// 			data = {
+// 				clickedPieceSquare: clickedPieceSquare,
+// 		    	updateGamesData: updateGamesData,
+// 		    	moveToSquareAndContents: moveToSquareAndContents
+// 		    }
+// 		    console.log(moveToSquareAndContents)
+// 		    console.log(clickedPieceSquare);
+// 			$.ajax({
+// 			    type: "PATCH",
+// 			    url: "/move",
+// 			    data: data,  //this is where I need to post the updated contents in games.id#.squares table.
+// 		    					// Also need to have a users table and update how many moves they made for that side in a 
+// 		    					// 	particular game.  Maybe with: Users has_many games through: newTable, Games has_many users through: newTable
+// 			    success: onSaveSuccessPatch,
+// 			    error: onSaveFailurePatch,
+// 			});
+// 		}
+// 	}
 
-	function moveToSquare (response) {
-		for (var i=1; i<9; i++) { // this is every div on my board right now.  I could change this to just the container div
-								  // I tried that actually and didnt work right anymore.  idk why
-			for (var j=1; j<9; j++) {					
-				$('.row' + i.toString() + '.column' + j.toString()).on('click', function(){
-					console.log(contents);
-					moveToSquareAndContents = {
-						x: $(this).attr('data-column'),
-						y: $(this).attr('data-row'),
-						contents: contents
-					}
-					moveToSquarePartTwo(response)
-				});
-			}
-		}
-	}
+// 	function moveToSquare (response) {
+// 		for (var i=1; i<9; i++) { // this is every div on my board right now.  I could change this to just the container div
+// 								  // I tried that actually and didnt work right anymore.  idk why
+// 			for (var j=1; j<9; j++) {					
+// 				$('.row' + i.toString() + '.column' + j.toString()).on('click', function(){
+// 					console.log(contents);
+// 					moveToSquareAndContents = {
+// 						x: $(this).attr('data-column'),
+// 						y: $(this).attr('data-row'),
+// 						contents: contents
+// 					}
+// 					moveToSquarePartTwo(response)
+// 				});
+// 			}
+// 		}
+// 	}
 
-	// function onSaveFailurePatchLayout () {
+// 	// function onSaveFailurePatchLayout () {
 
-	// }
+// 	// }
 
-	// function onSaveSuccessPatchLayout () {
+// 	// function onSaveSuccessPatchLayout () {
 
-	// }
+// 	// }
 
-	function onSaveFailurePatch (err) {
-		console.log("error Posting move to database");
-	}
+// 	function onSaveFailurePatch (err) {
+// 		console.log("error Posting move to database");
+// 	}
 
-	function onSaveSuccessPatch (response) {
-		console.log(response);
-		nextLayout ();
-		// appendPieces(response);
-	}
+// 	function onSaveSuccessPatch (response) {
+// 		console.log(response);
+// 		nextLayout ();
+// 		// appendPieces(response);
+// 	}
 
-	function onSaveFailure (err) {
-		console.log(err);
-		console.log("error getting chess pieces layout");
-	}
-	function onSaveSuccess (response) {
-		console.log(response);
-		appendPieces (response);	
-	}
-})
+// 	function onSaveFailure (err) {
+// 		console.log(err);
+// 		console.log("error getting chess pieces layout");
+// 	}
+// 	function onSaveSuccess (response) {
+// 		console.log(response);
+// 		appendPieces (response);	
+// 	}
+// })
--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -2,23 +2,24 @@ class SquaresController < ApplicationController
 
 	def get_layout
 		# gm = Game.first#find_by(id: 2)
-		gm = Game
-			.where(finished: false)
+		gm = Game.where(finished: false)
 			.where(active: false)
 			.order(created_at: :asc)
 			.limit(1)
+			puts "djhgkfdjgf"
+			p gm
+			puts "jsdkfjdkfg"
 		gm = gm[0]
 		#gm = Game.where(id is the lowest, finished == false, active == false)
 		gmId = gm.id
 		spaces = gm.squares.all
-		gm.update_attribute(:active, true)
+		# gm.update_attribute(:active, true)
 		
 		# and also is not the game that the user last made a move on
 		render json: {
 			:spaces => spaces, 
 			:gmId => gmId
 		}
-
 	end
 
 	def update
