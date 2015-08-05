3d13b00d3b69191f773bfdca88d119cd1cd2a1cc
Eddie Rendleman <skooltransformer2@gmail.com>
2015-08-05 16:31:01 -0400

    chat doesn't display anything now but its closer to being able to display comments sent by other users, instead of just displaying whatever the users own comments are

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,10 +1,18 @@
 class PhrasesController < ApplicationController
 	def create
 		words = params[:words]
+		puts "sdjhgjdgjfdgjfdhgjkfdngjdfgjfndjgnfdjkgnfdjkngjkdfngjkdfngjkfdj"
+		puts words
+		puts "jdhfjdshfjkdhgjkdfgjfdjghdfkjhgdfjhgkjfdhgjfdhgjhdfjghfdjhgd"
 		Phrase.create(:user_id => current_user.id, :words => words)
+		all_words_array = []
+		all_words = Phrase.all.each do |wrds|
+			all_words_array.push(wrds.words)
+		end
+		puts all_words
 		render json: {
 			:user_id => current_user,
-			:words => words 
+			:words => all_words_array 
 		}  #putting finishing this chat off until later
 	end
 end
---
+++ db/migrate/20150805202936_add_attribute_to_users.rb
@@ -0,0 +1,4 @@
+class AddAttributeToUsers < ActiveRecord::Migration
+  def change
+  end
+end
