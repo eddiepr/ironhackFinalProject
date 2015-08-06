096d4f0a668706f9dc7ab7ea12e5f6fc6c3d9e1e
Eddie Rendleman <skooltransformer2@gmail.com>
2015-08-06 15:12:14 -0400

    chat is displaying correct user for each comment rather than always displaying the current user for each comment

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,38 +1,29 @@
 class PhrasesController < ApplicationController
-	def create
-		words = params[:words]
-		puts "sdjhgjdgjfdgjfdhgjkfdngjdfgjfndjgnfdjkgnfdjkngjkdfngjkdfngjkfdj"
-		puts words
-		puts "jdhfjdshfjkdhgjkdfgjfdjghdfkjhgdfjhgkjfdhgjfdhgjhdfjghfdjhgd"
-		Phrase.create(:user_id => current_user.id, :words => words)
+	def array_and_json
 		all_words_array = []
 		all_words = Phrase.all.each do |wrds|
-			all_words_array.push(wrds.words)
+			id = wrds.user_id
+			this_user = User.find_by(id: id)
+			this_email = this_user.email
+			email_array = this_email.split("@")
+			email_prefix = email_array[0]
+			all_words_array.push(email_prefix + ":" + " " + wrds.words)
 		end
 		puts "all words array is below"
 		puts all_words_array
 		# test_array = [1,2,3,4,5,6,7,8]
 		# puts test_array
 		render json: {
-			:user_id => current_user.id,
-			:words => all_words_array,
-			:email => current_user.email 
+			:words => all_words_array
 		}  
+	end
+	def create
+		words = params[:words]
+		Phrase.create(:user_id => current_user.id, :words => words)
+		self.array_and_json
 	end
 
 	def update_display
-		all_words_array = []
-		all_words = Phrase.all.each do |wrds|
-			all_words_array.push(wrds.words)
-		end
-		puts "all words array is below"
-		puts all_words_array
-		# test_array = [1,2,3,4,5,6,7,8]
-		# puts test_array
-		render json: {
-			:user_id => current_user.id,
-			:words => all_words_array,
-			:email => current_user.email
-		}
+		self.array_and_json
 	end
 end
