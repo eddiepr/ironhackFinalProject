7761cdd5bb95563d9b8802dcbb79e3b714a2ba3c
Eddie Rendleman <skooltransformer2@gmail.com>
2015-08-06 17:19:25 -0400

    fixed syntax error in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,17 @@ class UsersController < ApplicationController
 		# 	id: gameId,
 		# 	finishedStatus: true
 		# }
-		data = params[:updateGamesData]
+		# data = params[:updateGamesData]
 
-		gameId = data[:id]
-		@gameo = Game.find_by(id: gameId)
-		@gameo.update_attribute(:finished, true)
-		@gameo.update_attribute(:active, true)
-		current_user.update_attribute(:)
-		game_turn = @gameo.turn
-		User.find_by(id: current_user.id)
-		turn = Turn.where("game_id = ? AND user_id = ? AND color = ?", @gameo, current_user.id, "black")   
-		turn_length = turn.length 
-		
+		# gameId = data[:id]
+		# @gameo = Game.find_by(id: gameId)
+		# @gameo.update_attribute(:finished, true)
+		# @gameo.update_attribute(:active, true)
+		# game_turn = @gameo.turn
+		# User.find_by(id: current_user.id)
+		# turn = Turn.where("game_id = ? AND user_id = ? AND color = ?", @gameo, current_user.id, "black")   
+		# turn_length = turn.length 
+
 		# Turn.create(:user_id => current_user.id, :game_id => @gameo.id, :color => @gameo.turn)
 
 		render :nothing => true #change this to render "join_game", and get rid
