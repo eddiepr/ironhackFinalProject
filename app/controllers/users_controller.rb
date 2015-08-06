570757d6fd9faeb9adddf5c91559e3c72388d196
Eddie Rendleman <skooltransformer2@gmail.com>
2015-08-06 14:01:27 -0400

    made the pieces append to the move to squares before the board goes away

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,24 +5,23 @@ class UsersController < ApplicationController
 	end
 
 	def update_points
-		updateGamesData = {
-			id: gameId,
-			finishedStatus: true
-		}
-
-
+		# updateGamesData = {
+		# 	id: gameId,
+		# 	finishedStatus: true
+		# }
 		data = params[:updateGamesData]
 
 		gameId = data[:id]
 		@gameo = Game.find_by(id: gameId)
 		@gameo.update_attribute(:finished, true)
 		@gameo.update_attribute(:active, true)
+		current_user.update_attribute(:)
 		game_turn = @gameo.turn
-
-
+		User.find_by(id: current_user.id)
+		turn = Turn.where("game_id = ? AND user_id = ? AND color = ?", @gameo, current_user.id, "black")   
+		turn_length = turn.length 
+		
 		# Turn.create(:user_id => current_user.id, :game_id => @gameo.id, :color => @gameo.turn)
-
-
 
 		render :nothing => true #change this to render "join_game", and get rid
 		#of the page reload in the js
